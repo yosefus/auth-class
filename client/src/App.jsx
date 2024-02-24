5aec79bb99b5efacfefb4299a75eaeb5af718323
yosefus <yosalsoni@gmail.com>
2024-02-24 22:00:07 +0200

    first

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -42,13 +42,23 @@ const HomePage = () => (<div className='p-12 h-full flex flex-col gap-4 items-ce
   <img src="/1.jpg" alt="home" />
   <button className='px-8 py-2 rounded-md bg-orange-500'>create axios req</button>
 </div>)
+
 const AdminPage = () => (<div className='p-12 h-full flex flex-col gap-4 items-center'>
   <h1>admin page</h1>
   <img src="/2.jpg" alt="admin" />
   <button className='px-8 py-2 rounded-md bg-orange-500'>create axios req</button>
 </div>)
+
 const LoginPage = () => {
-  return (<div className='p-6 md:p-12 flex items-center justify-center h-full'>
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const fd = new FormData(e.target)
+    const body = Object.fromEntries(fd)
+    console.log({ body });
+  }
+
+  return (<div onSubmit={handleSubmit} className='p-6 md:p-12 flex items-center justify-center h-full'>
     <form className='rounded-lg shadow-lg max-w-96 flex flex-col gap-4  p-6 w-full bg-black text-white'>
       <h1>Login</h1>
       <input type="email" name='email' placeholder='email...' />
@@ -57,8 +67,17 @@ const LoginPage = () => {
     </form>
   </div>)
 }
+
 const RegisterPage = () => {
-  return (<div className='p-6 md:p-12 flex items-center justify-center h-full'>
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const fd = new FormData(e.target)
+    const body = Object.fromEntries(fd)
+    console.log({ body });
+  }
+
+  return (<div onSubmit={handleSubmit} className='p-6 md:p-12 flex items-center justify-center h-full'>
     <form className='rounded-lg shadow-lg max-w-96 flex flex-col gap-4 p-6 w-full bg-black text-white'>
       <h1>Register</h1>
       <input type="email" name='email' placeholder='email...' />
